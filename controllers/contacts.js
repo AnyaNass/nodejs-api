0130d9678199d6b7d7bf972c3f4a73f89ef85d77
Anna <98224289+AnyaNass@users.noreply.github.com>
2022-12-23 16:48:44 +0200

    home work without extra tasks

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,10 @@ const { Contact } = require('../models/contact')
 const { HttpError, controllerWrapper } = require("../helpers")
 
 const getAll = async (req, res) => {
-	const result = await Contact.find();
+	const { _id: owner } = req.user;
+	const { page = 1, limit = 10 } = req.query;
+	const skip = (page - 1) * limit;
+	const result = await Contact.find({ owner }, "", { skip, limit }).populate("owner", "email _id")
 
 	res.status(200).json(result);
 }
@@ -21,7 +24,8 @@ const getById = async (req, res) => {
 }
 
 const addContact = async (req, res) => {
-	const result = await Contact.create(req.body);
+	const { _id: owner } = req.user;
+	const result = await Contact.create({ ...req.body, owner });
 	res.status(201).json(result)
 }
 
---
+++ models/user.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require("mongoose");
+const Joi = require('joi');
+const { handleMongooseError } = require("../helpers")
+
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+const userSchema = new Schema(
+	{
+		password: {
+			type: String,
+			required: [true, 'Password is required'],
+			minlength: 6,
+		},
+		email: {
+			type: String,
+			required: [true, 'Email is required'],
+			unique: true,
+			match: EMAIL_REGEXP,
+			unique: true,
+		},
+		subscription: {
+			type: String,
+			enum: ["starter", "pro", "business"],
+			default: "starter"
+		},
+		token: {
+			type: String,
+			default: null,
+		},
+	}, { versionKey: false }
+)
+
+userSchema.post("save", handleMongooseError);
+
+const signupSchema = Joi.object({
+	email: Joi.string().pattern(EMAIL_REGEXP).required(),
+	password: Joi.string().min(6).required()
+})
+
+const loginSchema = Joi.object({
+	email: Joi.string().pattern(EMAIL_REGEXP).required(),
+	password: Joi.string().min(6).required()
+})
+
+const schemas = {
+	signupSchema,
+	loginSchema
+}
+
+const User = model("user", userSchema);
+
+module.exports = {
+	User,
+	schemas
+}
